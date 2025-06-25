72bb37463bddda1ce4479356840f941093d3495c
MatteoFalcioni <matteo.falk@gmail.com>
2025-06-25 10:29:22 +0100

    REFACTORING + STARTING PROJECT

---
+++ LangChain/Interview_Questions_Creator/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="Matteo Falcioni",
+    author_email="matteo.falcioni3@unibo.it",
+    packages= find_packages(),
+    install_requires = []
+)
