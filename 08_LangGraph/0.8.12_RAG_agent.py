660fb6879cd2301faa1d3c27ecb0f073c925e76e
MatteoFalcioni <matteo.falk@gmail.com>
2025-06-27 14:43:13 +0200

    over with langgraoh basics

--- 08_LangGraph/0.8.12_RAG_agent.py
+++ 08_LangGraph/0.8.12_RAG_agent.py
@@ -48,7 +48,7 @@ text_splitter = RecursiveCharacterTextSplitter(
 
 pages_split = text_splitter.split_documents(pages) # We now apply this to our pages
 
-persist_directory = r"C:\Vaibhav\LangGraph_Book\LangGraphCourse\Agents"
+persist_directory = r"../08_LangGraph"
 collection_name = "stock_market"
 
 # If our collection does not exist in the directory, we create using the os command
@@ -72,7 +72,143 @@ except Exception as e:
 
 
 # Now we create our retriever 
-retriever = vectorstore.as_retriever(
+retriever = vectorstore.as_retriever(       # default retriever
     search_type="similarity",
     search_kwargs={"k": 5} # K is the amount of chunks to return
 )
+
+
+@tool
+def retriever_tool(query: str) -> str:
+    """
+    This tool searches and returns the information from the Stock Market Performance 2024 document.
+    """
+
+    docs = retriever.invoke(query)
+
+    if not docs:
+        return "I found no relevant information in the Stock Market Performance 2024 document."
+    
+    results = []
+    for i, doc in enumerate(docs):
+        results.append(f"Document {i+1}:\n{doc.page_content}")
+    
+    return "\n\n".join(results)
+
+
+tools = [retriever_tool]
+
+llm = llm.bind_tools(tools)
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+
+def should_continue(state: AgentState):
+    """Check if the last message contains tool calls."""
+    result = state['messages'][-1]
+    return hasattr(result, 'tool_calls') and len(result.tool_calls) > 0
+
+
+system_prompt = """
+You are an intelligent AI assistant who answers questions about Stock Market Performance in 2024 based on the PDF document loaded into your knowledge base.
+Use the retriever tool available to answer questions about the stock market performance data. You can make multiple calls if needed.
+If you need to look up some information before asking a follow up question, you are allowed to do that!
+Please always cite the specific parts of the documents you use in your answers.
+"""
+
+
+tools_dict = {our_tool.name: our_tool for our_tool in tools} # Creating a dictionary of our tools
+
+
+# LLM Agent
+def call_llm(state: AgentState) -> AgentState:
+    """Function to call the LLM with the current state."""
+    messages = list(state['messages'])
+    messages = [SystemMessage(content=system_prompt)] + messages
+    message = llm.invoke(messages)
+    return {'messages': [message]}
+
+
+# Retriever Agent
+def take_action(state: AgentState) -> AgentState:
+    """Execute tool calls from the LLM's response."""
+
+    tool_calls = state['messages'][-1].tool_calls
+    results = []
+    for t in tool_calls:
+        print(f"Calling Tool: {t['name']} with query: {t['args'].get('query', 'No query provided')}")
+        
+        if not t['name'] in tools_dict: # Checks if a valid tool is present
+            print(f"\nTool: {t['name']} does not exist.")
+            result = "Incorrect Tool Name, Please Retry and Select tool from List of Available tools."
+        
+        else:
+            result = tools_dict[t['name']].invoke(t['args'].get('query', ''))
+            print(f"Result length: {len(str(result))}")
+
+        # Appends the Tool Message
+        results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
+
+    print("Tools Execution Complete. Back to the model!")
+    return {'messages': results}
+
+
+graph = StateGraph(AgentState)
+graph.add_node("llm", call_llm)
+graph.add_node("retriever_agent", take_action)
+
+graph.add_conditional_edges(
+    "llm",
+    should_continue,
+    {
+        True: "retriever_agent", 
+        False: END
+    }
+)
+graph.add_edge("retriever_agent", "llm")
+graph.set_entry_point("llm")
+
+rag_agent = graph.compile()
+
+
+def running_agent():
+    print("\n=== RAG AGENT===")
+    
+    while True:
+        user_input = input("\nWhat is your question: ")
+        if user_input.lower() in ['exit', 'quit']:
+            break
+            
+        messages = [HumanMessage(content=user_input)] # converts back to a HumanMessage type
+
+        result = rag_agent.invoke({"messages": messages})
+        
+        print("\n=== ANSWER ===")
+        print(result['messages'][-1].content)
+
+
+running_agent()
+
+
+"""
+(openAI) (CNNLidar) matteo@pcmatteo:~/Learning_AI/GenAI/08_LangGraph$ python 0.8.12_RAG_agent.py 
+PDF has been loaded and has 9 pages
+Created ChromaDB vector store!
+
+=== RAG AGENT===
+
+What is your question: What can you tell me in a summary of the 2024 stock market
+Calling Tool: retriever_tool with query: 2024 stock market summary
+Result length: 4664
+Tools Execution Complete. Back to the model!
+
+=== ANSWER ===
+In 2024, the U.S. stock market experienced a remarkably strong performance, with the S&P 500 index delivering a total return of approximately 25%, marking the second consecutive year of over 20% returns. The Nasdaq Composite outpaced the broader market with a nearly 29% increase. However, smaller-cap stocks had more modest gains, with the S&P 500 Equal-Weight index and the Russell 2000 rising about 10-11%.
+
+The year was characterized by the dominance of mega-cap technology stocks, often referred to as the "Magnificent 7," which included companies like Apple, Microsoft, Alphabet (Google), Amazon, Meta, Nvidia, and Tesla. These companies collectively surged by roughly 64-67% on average and accounted for over half of the S&P 500's gains. The technology sector, particularly those tied to transformational trends such as artificial intelligence, cloud computing, and quantum technology, led the market rally.
+
+Despite the strong performance, the market's gains were not evenly distributed, with traditional and value-oriented sectors lagging behind. By the end of the year, many leading stocks were trading at high valuations, reflecting significant optimism for future growth. The exuberance of 2024 created a market environment driven by technology and growth stocks, with a focus on the largest companies. Investors remained optimistic yet cautious about potential corrections due to the rapid rise in stock prices.
+
+What is your question: 
+"""
